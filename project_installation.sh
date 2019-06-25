4ea5f63ab18a749108b492cb14c9c9c16084a1a9
Dophy6 <fabio.merola95@gmail.com>
2019-06-25 18:27:56 +0200

    shell script made to make one tap installation

---
+++ project_installation.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+echo "Install requirements"
+
+sudo apt update
+sudo apt-get install p7zip
+sudo apt  install jq
+sudo apt install mysql-server
+
+sudo mysql <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'progetto_distribuiti19';
+FLUSH PRIVILEGES;
+exit;
+EOF
+
+systemctl enable mysql.service
+sudo apt-get install python3
+sudo apt install python3-pip
+pip install mysql-connector-python
+pip install multiprocessing
+
+DOWNLOAD_PATH = jq .INSTALLATION.DOWNLOAD_PATH config.json
+# Downloading dump
+
+echo "Start to download Posts.xml.7z"
+wget  -P DOWNLOAD_PATH "https://zenodo.org/record/2628274/files/Posts.xml.7z?download=1"
+echo "Start to download PostReferenceGH.csv.7z"
+wget  -P DOWNLOAD_PATH "https://zenodo.org/record/2628274/files/PostReferenceGH.csv.7z?download=1"
+echo "Start to download PostLinks.xml.7z"
+wget  -P DOWNLOAD_PATH "https://zenodo.org/record/2628274/files/PostLinks.xml.7z?download=1"
+echo "Start to download Comments.xml.7z"
+wget  -P DOWNLOAD_PATH "https://zenodo.org/record/2628274/files/Comments.xml.7z?download=1"
+
+echo "Un-zipping and deleting dump.7z"
+
+7za e $DOWNLOAD_PATH$"Posts.xml.7z"
+rm $DOWNLOAD_PATH$"Posts.xml.7z"
+7za e $DOWNLOAD_PATH$"PostReferenceGH.csv.7z"
+rm $DOWNLOAD_PATH$"PostReferenceGH.csv.7z"
+7za e $DOWNLOAD_PATH$"PostLinks.xml.7z"
+rm $DOWNLOAD_PATH$"PostLinks.xml.7z"
+7za e $DOWNLOAD_PATH$"Comments.xml.7z"
+rm $DOWNLOAD_PATH$"Comments.xml.7z"
+
+echo "Creating database and importing data"
+
+mysql -u root -pprogetto_distribuiti19 <<EOF
+CREATE DATABASE SistemiDistribuiti;
+USE SistemiDistribuiti;
+CREATE TABLE Posts (Id INT(11) NOT NULL PRIMARY KEY, PostTypeId TINYINT(4), AcceptedAnswerId INT(11), ParentId INT(11), CreationDate DATETIME, DeletionDate DATETIME, Score INT(11), ViewCount INT(11), Body TEXT, OwnerUserId INT(11), OwnerDisplayName VARCHAR(40), LastEditorUserId INT(11), LastEditorDisplayName VARCHAR(40), LastEditDate DATETIME, LastActivityDate DATETIME, Title VARCHAR(250), Tags VARCHAR(150), AnswerCount INT(11), CommentCount INT(11), FavoriteCount INT(11), ClosedDate DATETIME, CommunityOwnedDate DATETIME);
+CREATE TABLE Comments (Id INT(11) NOT NULL PRIMARY KEY, PostId INT(11), Score INT(11), Text TEXT, CreationDate DATETIME, UserDisplayName VARCHAR(40), UserId INT(11));
+CREATE TABLE PostLinks (Id INT(11) NOT NULL PRIMARY KEY, CreationDate DATETIME, PostId INT(11), RelatedPostId INT(11), LinkTypeId TINYINT(4));
+CREATE TABLE PostReferenceGH (Id INT(11) NOT NULL PRIMARY KEY, FileId VARCHAR(40), Repo VARCHAR(255), RepoOwner VARCHAR(255), RepoName VARCHAR(255), Branch VARCHAR(255), Path TEXT, FileExt VARCHAR(255), Size INT(11), Copies INT(11), PostId INT(11), PostTypeId TINYINT(4), CommentId INT(11), SOUrl TEXT, GHUrl TEXT);
+LOAD XML LOCAL INFILE $DOWNLOAD_PATH$'Comments.xml' INTO TABLE Comments ROWS IDENTIFIED BY '<row>';
+LOAD XML LOCAL INFILE $DOWNLOAD_PATH$'Posts.xml' INTO TABLE Posts ROWS IDENTIFIED BY '<row>';
+LOAD XML LOCAL INFILE $DOWNLOAD_PATH$'PostLinks.xml' INTO TABLE PostLinks ROWS IDENTIFIED BY '<row>';
+LOAD DATA LOCAL INFILE $DOWNLOAD_PATH$'PostReferenceGH.csv' INTO TABLE PostReferenceGH FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS (FileId, Repo, RepoOwner, RepoName, Branch, Path, FileExt, Size, Copies, PostId, @CommentId, SOUrl, GHUrl) SET CommentId = nullif(@CommentId, '');
+ALTER TABLE Posts ADD INDEX (Date)
+ALTER TABLE Posts ADD INDEX (ParentId) 
+ALTER TABLE PostLink ADD INDEX (PostId) 
+ALTER TABLE Comments ADD INDEX (PostId) 
+ALTER TABLE PostReferenceGH ADD INDEX (PostId)
+EOF
+
+echo "Deleting dump files"
+
+rm $DOWNLOAD_PATH$"Posts.xml"
+rm $DOWNLOAD_PATH$"PostReferenceGH.csv"
+rm $DOWNLOAD_PATH$"PostLinks.xml"
+rm $DOWNLOAD_PATH$"Comments.xml"
+
+echo "Start script to build sliced db in csv"
+
+python3 database_maker_in_csv.py
+
+echo "Recreate database from csv"
+
+mysql -u root -pprogetto_distribuiti19 <<EOF
+USE SistemiDistribuiti;
+DROP TABLE [IF EXIST] [Posts], [Comments], [PostLinks], [PostReferenceGH];
+CREATE TABLE Questions (Id INT(11) NOT NULL PRIMARY KEY, PostTypeId TINYINT(4), AcceptedAnswerId INT(11), ParentId INT(11), CreationDate DATETIME, DeletionDate DATETIME, Score INT(11), ViewCount INT(11), Body TEXT, OwnerUserId INT(11), OwnerDisplayName VARCHAR(40), LastEditorUserId INT(11), LastEditorDisplayName VARCHAR(40), LastEditDate DATETIME, LastActivityDate DATETIME, Title VARCHAR(250), Tags VARCHAR(150), AnswerCount INT(11), CommentCount INT(11), FavoriteCount INT(11), ClosedDate DATETIME, CommunityOwnedDate DATETIME);
+CREATE TABLE Answers (Id INT(11) NOT NULL PRIMARY KEY, PostTypeId TINYINT(4), AcceptedAnswerId INT(11), ParentId INT(11), CreationDate DATETIME, DeletionDate DATETIME, Score INT(11), ViewCount INT(11), Body TEXT, OwnerUserId INT(11), OwnerDisplayName VARCHAR(40), LastEditorUserId INT(11), LastEditorDisplayName VARCHAR(40), LastEditDate DATETIME, LastActivityDate DATETIME, Title VARCHAR(250), Tags VARCHAR(150), AnswerCount INT(11), CommentCount INT(11), FavoriteCount INT(11), ClosedDate DATETIME, CommunityOwnedDate DATETIME);
+CREATE TABLE Comments (Id INT(11) NOT NULL PRIMARY KEY, PostId INT(11), Score INT(11), Text TEXT, CreationDate DATETIME, UserDisplayName VARCHAR(40), UserId INT(11));
+CREATE TABLE PostLinks (Id INT(11) NOT NULL PRIMARY KEY, CreationDate DATETIME, PostId INT(11), RelatedPostId INT(11), LinkTypeId TINYINT(4));
+CREATE TABLE PostReferenceGH (Id INT(11) NOT NULL PRIMARY KEY, FileId VARCHAR(40), Repo VARCHAR(255), RepoOwner VARCHAR(255), RepoName VARCHAR(255), Branch VARCHAR(255), Path TEXT, FileExt VARCHAR(255), Size INT(11), Copies INT(11), PostId INT(11), PostTypeId TINYINT(4), CommentId INT(11), SOUrl TEXT, GHUrl TEXT);
+LOAD DATA LOCAL INFILE 'questions.csv' INTO TABLE Questions FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'answers.csv' INTO TABLE Answers FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'comments.csv' INTO TABLE Comments FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'postlinks.csv' INTO TABLE PostLinks FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE 'postreferGH.csv' INTO TABLE PostReferenceGH FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n';
+ALTER TABLE Questions ADD FULLTEXT (Title);
+ALTER TABLE Answers ADD INDEX (ParentId);
+ALTER TABLE Answers ADD FULLTEXT (Body);
+ALTER TABLE PostLinks ADD INDEX (RelatedPostId); 
+ALTER TABLE Comments ADD INDEX (PostId); 
+ALTER TABLE Comments ADD FULLTEXT (Text); 
+ALTER TABLE PostReferenceGH ADD INDEX (PostId);
+EOF
